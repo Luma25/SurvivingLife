e752e8200c197f62f2d8606d7d46e9535fc8b05f
Luis Cáceres <luimacaroster@gmail.com>
2018-12-03 18:50:08 -0300

    source

---
+++ Source/SurvivingLife/SurvivingLifeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivingLifeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASurvivingLifeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASurvivingLifeGameMode();
+};
+
+
+
