7621bc14876dd1d8a4bc15c9ad419d61f13da65d
Luis Cáceres <luimacaroster@gmail.com>
2018-12-05 18:20:40 -0300

    its working... ITS WORKING!

---
+++ Intermediate/Build/Mac/UE4Editor/Inc/SurvivingLife/SurvivingLifeGameMode.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SURVIVINGLIFE_SurvivingLifeGameMode_generated_h
+#error "SurvivingLifeGameMode.generated.h already included, missing '#pragma once' in SurvivingLifeGameMode.h"
+#endif
+#define SURVIVINGLIFE_SurvivingLifeGameMode_generated_h
+
+#define SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_RPC_WRAPPERS
+#define SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASurvivingLifeGameMode(); \
+	friend SURVIVINGLIFE_API class UClass* Z_Construct_UClass_ASurvivingLifeGameMode(); \
+public: \
+	DECLARE_CLASS(ASurvivingLifeGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/SurvivingLife"), SURVIVINGLIFE_API) \
+	DECLARE_SERIALIZER(ASurvivingLifeGameMode) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesASurvivingLifeGameMode(); \
+	friend SURVIVINGLIFE_API class UClass* Z_Construct_UClass_ASurvivingLifeGameMode(); \
+public: \
+	DECLARE_CLASS(ASurvivingLifeGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/SurvivingLife"), SURVIVINGLIFE_API) \
+	DECLARE_SERIALIZER(ASurvivingLifeGameMode) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	SURVIVINGLIFE_API ASurvivingLifeGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASurvivingLifeGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(SURVIVINGLIFE_API, ASurvivingLifeGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASurvivingLifeGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	SURVIVINGLIFE_API ASurvivingLifeGameMode(ASurvivingLifeGameMode&&); \
+	SURVIVINGLIFE_API ASurvivingLifeGameMode(const ASurvivingLifeGameMode&); \
+public:
+
+
+#define SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	SURVIVINGLIFE_API ASurvivingLifeGameMode(ASurvivingLifeGameMode&&); \
+	SURVIVINGLIFE_API ASurvivingLifeGameMode(const ASurvivingLifeGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(SURVIVINGLIFE_API, ASurvivingLifeGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASurvivingLifeGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ASurvivingLifeGameMode)
+
+
+#define SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_9_PROLOG
+#define SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_RPC_WRAPPERS \
+	SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_INCLASS \
+	SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SurvivingLife_Source_SurvivingLife_SurvivingLifeGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
